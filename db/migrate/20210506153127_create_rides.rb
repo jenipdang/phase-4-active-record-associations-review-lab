24a2532515350fe7647db9b0132d30668b0f8264
Jeni Dang <jenipdang@yahoo.com>
2022-04-13 11:52:30 -0700

    commit tree f90501fbc321ce53ad20d0c17e5fa52371f3bd66

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.belongs_to :taxi, null: false, foregin_key: true
+      t.belongs_to :passenger, null: false, foregin_key: true
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pass1 = Passenger.create(name: "Francis Dang")
+pass2 = Passenger.create(name: Faker::Name.unique.name)
+pass3 = Passenger.create(name: Faker::Name.unique.name)
+
+
+tax1 = Taxi.create(name: "Abdu Wallace")
+tax2 = Taxi.create(name: Faker::Name.unique.name)
+tax3 = Taxi.create(name: Faker::Name.unique.name)
+
+Ride.create(passenger_id: pass1.id, taxi_id: tax1.id)
+Ride.create(passenger_id: pass2.id, taxi_id: tax1.id)
+Ride.create(passenger_id: pass3.id, taxi_id: tax2.id)
+Ride.create(passenger_id: pass3.id, taxi_id: tax3.id)
